8fbb161d8d48b7cbc3394385287aa8e981d0027d
Ipek <72508587+Eldekipek@users.noreply.github.com>
2021-10-19 00:38:34 +0300

    18 Ekim  Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba Kodlama.io")
+
+// var dolarDun = 9.20
+// dolarDun = "Ankara" //console log da cıktı ankarayı gösterir.
+// var dolarBugun = 9.30
+
+// console.log("dolarBugun")
+
+// let dolarBugun = 9.30
+// let dolarDun = 9.20
+
+// {
+//     let dolarDun = 9.10
+// }
+// console.log(dolarDun)//cıktı kapsam dısı olan degisken degerini algılar 
+// //**fakat var ile tanımlasaydık kapsam icindeki degeri cıktı olarak alırdık 
+
+// const euroDun = 11.2
+// //euroDun =11 //cıktıda calısmaz 
+// console.log(euroDun)
+
+//array 
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i=0; i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+
+console.log(konutKredileri)
